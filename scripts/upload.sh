76aa8d5c1372666762407b07a45e475a1d5eb1da
Jorge Sanz <jorge.sanz@elastic.co>
2022-02-09 19:05:17 +0100

    initial import

---
+++ scripts/upload.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+log()
+{
+    echo "[$(date --iso-8601=minutes)] $1"
+}
+
+upload_dataset()
+{
+    layer=$1
+
+    index_name=${ES_INDEX}_${layer}
+
+    # Deleting if index exists
+    if $(curl -sI "${ES_URL}/${index_name}" | grep "HTTP/2 200" > /dev/null); then
+        log "Deleting {${index_name}} exists..."
+        curl -s -XDELETE "${ES_URL}/${index_name}"
+        echo ""
+    fi
+
+    log "Uploading the data into the ${index_name} index..."
+    ogr2ogr -progress -skipfailures\
+        -lco INDEX_NAME=${index_name} \
+        -lco MAPPING=/app/mappings/openstreetmap_${layer}.json \
+        ES:${ES_URL} \
+        ${DATA_FILE} ${layer}
+    
+    echo "-----------------------------"
+}
+
+
+DATA_FILE=/app/data/data.pbf
+
+if [ ! -e ${DATA_FILE} ]; then
+  echo "Data file {${DATA_FILE}} not found!"
+fi
+
+# Generate the points index
+
+log "Starting process to upload data from ${DOWNLOAD_AREA}"
+
+
+for layer in points lines multilinestrings multipolygons other_relations
+do
+    upload_dataset $layer
+done
+
+log "Done!"
